171c3bc9974212dd77fb0fe947d6eeb6ecba14f7
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-04-16 10:51:24 -0400

    Combining clients into a single class.

--- philter-sdk-net/PhilterClient.cs
+++ philter-sdk-net/PhilterClient.cs
@@ -238,7 +238,114 @@ namespace Philter
             throw new ClientException("Unable to get status.", response.ErrorException);
 
         }
+        
+                /// <summary>
+        /// Get the names of available filter profiles.
+        /// </summary>
+        /// <returns>A list of filter profile names.</returns>
+        /// <exception cref="ClientException"></exception>
+        public List<string> GetFilterProfiles()
+        {
 
+            var request = new RestRequest("api/profiles", Method.GET);
+            request.AddHeader("accept", "application/json");
+
+            if (_token != null)
+            {
+                request.AddHeader("Authorization", Base64Encode("token:" + _token));
+            }
+            
+            var response = _client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<List<string>>(response.Content);
+            }
+            
+            throw new ClientException("Unable to get filter profiles.", response.ErrorException);
+
+        }
+
+        /// <summary>
+        /// Get a filter profile.
+        /// </summary>
+        /// <param name="filterProfileName">The name of a filter profile.</param>
+        /// <returns>The filter profile.</returns>
+        /// <exception cref="ClientException"></exception>
+        public string GetFilterProfile(string filterProfileName)
+        {
+
+            var request = new RestRequest("api/profiles/{filterProfileName}", Method.GET);
+            request.AddParameter("filterProfileName", filterProfileName, ParameterType.UrlSegment);
+            request.AddHeader("accept", "application/json");
+            
+            if (_token != null)
+            {
+                request.AddHeader("Authorization", Base64Encode("token:" + _token));
+            }
+
+            var response = _client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                return response.Content;
+            }
+
+            throw new ClientException("Unable to get filter profile.", response.ErrorException);
+
+        }
+
+        /// <summary>
+        /// Upload a filter profile. If a filter profile with the same name already exists it will be overwritten.
+        /// </summary>
+        /// <param name="filterProfile">The filter profile.</param>
+        /// <exception cref="ClientException"></exception>
+        public void SaveFilterProfile(string filterProfile)
+        {
+
+            var request = new RestRequest("api/profiles", Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", filterProfile, ParameterType.RequestBody);
+            
+            if (_token != null)
+            {
+                request.AddHeader("Authorization", Base64Encode("token:" + _token));
+            }
+
+            var response = _client.Execute(request);
+
+            if (!response.IsSuccessful)
+            {
+                throw new ClientException("Unable to save filter profile.", response.ErrorException);
+            }
+
+        }
+
+        /// <summary>
+        /// Delete a filter profile.
+        /// </summary>
+        /// <param name="filterProfileName">The name of the filter profile to delete.</param>
+        /// <exception cref="ClientException"></exception>
+        public void DeleteFilterProfile(string filterProfileName)
+        {
+
+            var request = new RestRequest("api/profiles/{filterProfileName}", Method.DELETE);
+            request.AddParameter("filterProfileName", filterProfileName, ParameterType.UrlSegment);
+            
+            if (_token != null)
+            {
+                request.AddHeader("Authorization", Base64Encode("token:" + _token));
+            }
+
+            var response = _client.Execute(request);
+
+            if (!response.IsSuccessful)
+            {
+                throw new ClientException("Unable to delete filter profile.", response.ErrorException);
+            }
+
+        }
+        
         private string Base64Encode(string plainText)
         {
             return System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(plainText));
